51f5983abd97dccdad3c00dc723f7cc92f9f5edd
nobel7761 <habiburnobel@gmail.com>
2023-02-08 13:02:45 +0600

    facebook added

--- src/Components/LowerSection.tsx
+++ src/Components/LowerSection.tsx
@@ -5,6 +5,8 @@ import { MdWork } from "react-icons/md";
 import { BsWhatsapp } from "react-icons/bs";
 import { TfiLinkedin } from "react-icons/tfi";
 import { BsGithub } from "react-icons/bs";
+import { ImFacebook } from "react-icons/im";
+import Link from "next/link";
 
 const LowerSection = () => {
   return (
@@ -36,9 +38,22 @@ const LowerSection = () => {
       <div className="pl-[68px]">
         <p className="mb-6">Social Media</p>
         <div className="flex gap-x-4 items-center">
-          <div className="bg-[#1BD741] w-8 h-8 rounded-full flex justify-center items-center">
-            <BsWhatsapp className="text-white" />
-          </div>
+          {/* facebook */}
+          <Link
+            href="https://www.facebook.com/habibur.rahaman.nobel"
+            target="_blank"
+          >
+            <div className="bg-[#0034B7] w-8 h-8 rounded-full flex justify-center items-center">
+              <ImFacebook className="text-white" />
+            </div>
+          </Link>
+          {/* whatsapp */}
+          <Link href="https://wa.me/+8801521320912" target="_blank">
+            <div className="bg-[#1BD741] w-8 h-8 rounded-full flex justify-center items-center">
+              <BsWhatsapp className="text-white" />
+            </div>
+          </Link>
+
           <div className="bg-[#0077B5] w-8 h-8 rounded-full flex justify-center items-center">
             <TfiLinkedin className="text-white" />
           </div>
