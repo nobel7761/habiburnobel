66f205b6f78e5db0c9457df4d736642e175b21c2
nobel7761 <habiburnobel@gmail.com>
2023-02-20 15:53:20 +0600

    root page modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,18 @@ import TopSection from "../src/Components/ViewCard/TopSection";
 
 const index = () => {
   return (
-    <div className="flex flex-col gap-y-3">
-      <h1>base page</h1>
+    <div className="flex flex-col gap-y-2 h-screen justify-center items-center">
+      <h1>Welcome To Super Portfolio</h1>
       <Link href="/viewcard">
-        <button>Go to View Card</button>
+        <button className="px-10 py-2 rounded bg-[#3460FB] bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500 text-white">
+          View Card
+        </button>
+      </Link>
+
+      <Link href="/profilecard">
+        <button className="px-10 py-2 rounded bg-[#3460FB] bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500 text-white">
+          Profile Card
+        </button>
       </Link>
     </div>
   );
